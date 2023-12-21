e3c59eb85dc5a74aae957dce986d3055b2016c49
abdul mohiz khan <abdulmohizmohizkhan@gmail.com>
2023-12-21 20:55:09 +0500

    sepiaGreen

--- colorUtils.c
+++ colorUtils.c
@@ -10,8 +10,56 @@ int max(int x, int y, int z) {
   m = m > z ? m : z;
   return m;
 }
-
+int min(int a,int b,int c)
+{
+  int min=a;
+  if(b<min)
+  {
+    min=b;
+  }
+  if(c<min)
+  {
+    min=c;
+  }
+  return min;
+}
 int toGrayScaleAverage(int r, int g, int b) {
   //TODO: test this, it may be wrong!
   return (r + g + b) / 3;
+}
+
+int toGrayScaleLightness(int red,int green,int blue)
+{
+  return(max(red,green,blue)+min(red,green,blue))/2;
+}
+int toGrayScaleLuminosity(int red,int green,int blue)
+{
+  return (int)(0.21*red+0.72*green+0.07*blue);//using (int) for rounding of!!
+}
+int toSepiaRed(int red,int green,int blue)
+{
+  int SepiaRed=(int)(0.393*red+0.769*green+0.189*blue);
+  if(SepiaRed>=255)
+  {
+    return 255;
+  }
+  return SepiaRed;
+}
+int toSepiaGreen(int red,int green,int blue)
+{
+  int SepiaGreen=(int)(0.349*red+0.686*green+0.168*blue);
+  if(SepiaGreen>=255)
+  {
+    return 255;
+  }
+  return SepiaGreen;
+}
+int toSepiaBlue(int red,int green,int blue)
+{
+  int SepiaBlue=(int)(0.272*red+0.534*green+0.131*blue);
+  if(SepiaBlue>=255)
+  {
+    return 255;
+  }
+  return SepiaBlue;
 }
